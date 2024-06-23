b7b780ffa6827fe982ddd9d883968d44dcd1fbbc
CleanIce <LHTS_TeamEmail@163.com>
2024-06-23 17:36:45 +0800

    R_Stack

---
+++ target/CMakeFiles/ERTC.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ERTC.dir/src/exceptions.cpp.obj"
+  "CMakeFiles/ERTC.dir/src/exceptions.cpp.obj.d"
+  "CMakeFiles/ERTC.dir/src/object.cpp.obj"
+  "CMakeFiles/ERTC.dir/src/object.cpp.obj.d"
+  "libERTC.a"
+  "libERTC.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ERTC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ target/CMakeFiles/main_test.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main_test.dir/test/main_test.cpp.obj"
+  "CMakeFiles/main_test.dir/test/main_test.cpp.obj.d"
+  "libmain_test.dll.a"
+  "main_test.exe"
+  "main_test.exe.manifest"
+  "main_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
